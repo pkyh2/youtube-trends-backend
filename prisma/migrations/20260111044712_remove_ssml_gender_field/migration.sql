9f491371470ccde1a4d98b6e4d678acce4e13038
pkyh2 <pkyh1@naver.com>
2026-01-11 13:55:54 +0900

    fix: ssmlGender 필드 다시 추가

---
+++ prisma/migrations/20260111044712_remove_ssml_gender_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ssml_gender` on the `shorts_story_voices` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "shorts_story_voices" DROP COLUMN "ssml_gender";
---
+++ prisma/migrations/20260111045530_add_re_ssml_gender_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "shorts_story_voices" ADD COLUMN     "ssml_gender" TEXT;
